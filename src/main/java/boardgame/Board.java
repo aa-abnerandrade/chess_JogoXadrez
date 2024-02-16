7e15fd68aae6dec28e4258ba32c9ef8192061363
Abner Andrade <aa.abnerandrade@gmail.com>
2024-02-15 21:27:19 -0300

    developing Board

--- src/main/java/boardgame/Board.java
+++ src/main/java/boardgame/Board.java
@@ -16,10 +16,17 @@ public class Board {
   private Piece[][] pieces;
 
 
-
   public Board(int rows, int columns) {
     this.rows = rows;
     this.columns = columns;
     this.pieces = new Piece[rows][columns];
+  }
+
+  public Piece getSquareFromPieces(int row, int column) {
+    return this.pieces[row][column];
+  }
+
+  public Piece getSquareFromPieces(Position position) {
+    return this.pieces[position.getRow()][position.getColumn()];
   }
 }
