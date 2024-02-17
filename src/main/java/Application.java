c733f28bccd2e7056546ec341a98fe21a0ac95eb
Abner Andrade <aa.abnerandrade@gmail.com>
2024-02-16 21:24:23 -0300

    adjusts

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,6 @@ import chess.ChessMatch;
 public class Application {
 
   public static void main(String[] args) {
-
     ChessMatch chessMatch = new ChessMatch();
     UserInterface.printBoard(chessMatch.getPieces());
   }
